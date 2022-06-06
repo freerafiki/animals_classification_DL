b3f5f1a0ab2aea6912389376816df2213276c935
freerafiki <bagigi@disroot.org>
2022-06-06 15:16:02 +0200

    tensorflow classification works

--- classification/ffnn/classify_tf.py
+++ classification/ffnn/classify_tf.py
@@ -1,7 +1,6 @@
 """
 Animals classification using Tensorflow and Keras.
 """
-
 from load_animals.load_random_animals import load_random_animals # loading the animals
 import pdb # debugging
 # TensorFlow and tf.keras
@@ -11,16 +10,21 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
 import numpy as np
-
-
+import matplotlib.pyplot as plt
 """
 LOADING THE DATA
 """
+img_width = 32
+img_height = 32
 train_x_orig, train_y, test_x_orig, test_y, classes = load_random_animals(
     dataset_folder = '/home/palma/opencampus/animals_classification_DL/dataset',
-    max_classes=6,
-    img_size=(256, 256),
+    max_classes=5,
+    img_size=(img_width, img_height),
     train_test_split=0.9)
+
+train_y = tf.keras.utils.to_categorical(np.transpose(train_y))
+test_y = tf.keras.utils.to_categorical(np.transpose(test_y))
+print(f"Data shapes:\nX: {train_x_orig.shape}\nY: {train_y.shape}")
 
 """
 CREATE THE MODEL
@@ -42,19 +46,18 @@ layers.Dropout(rate)
 Rescaling the images could be done with
 layers.Rescaling(scaling_factor, input_shape=(img_height, img_width, 3))
 """
-random_image = train_x_orig[100]
-np.max(random_image)
-image_shape = (256, 256, 3)
+image_shape = (img_width, img_height, 3)
 model = Sequential([
     # .. add here the layers
     layers.Flatten(input_shape=image_shape),
-    layers.Rescaling(1/256 ),
+    layers.Rescaling(1/255),
     layers.Dense(64, activation='relu'),
-    layers.Dropout(0.2),
+    layers.Dropout(0.5),
     layers.Dense(16, activation='relu'),
+    layers.Dropout(0.5),
     layers.Dense(8, activation='relu'),
-
-    layers.Dense(3, activation ='sigmoid')
+    layers.Dropout(0.5),
+    layers.Dense(5, activation ='sigmoid')
 ])
 
 # print out the model
@@ -69,36 +72,47 @@ Then feed the model with the data
 model.compile(
     # choose optimizer, loss and metrics
     optimizer = tf.keras.optimizers.Adam(
-        learning_rate=0.005,
-        # beta_1=0.9,
-        # beta_2=0.999,
-        # epsilon=1e-07,
-        # amsgrad=False,
-        name='Adam'
+        learning_rate=0.0001
     ),
-    loss = tf.keras.losses.CategoricalCrossentropy(
-        # from_logits=False,
-        # label_smoothing=0.0,
-        # axis=-1,
-        # name='categorical_crossentropy'
-    ),
-    metrics = ["acc"]
+    loss = tf.keras.losses.CategoricalCrossentropy(),
+    metrics = ["accuracy"]
 )
 
-epochs = 100
+epochs = 500
 
-train_x_orig.shape
-train_y.shape
-train_y_t = np.transpose(train_y)
-train_y_t.shape
-categorical_y = tf.keras.utils.to_categorical(train_y_t)
-categorical_y
-model.fit(
+
+history = model.fit(
     # choose images, labels and epochs
-    train_x_orig,
-    categorical_y,
+    x = train_x_orig,
+    y = train_y,
+    validation_data = (test_x_orig, test_y),
     epochs = epochs
 )
+
+def plot_training_evolution(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(32, 10))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+plot_training_evolution(history)
 
 """
 EVALUATE
@@ -109,8 +123,23 @@ Make some predictions
 model.evaluate(
     # choose test images and labels
     test_x_orig,
-    tf.keras.utils.to_categorical(np.transpose(test_y))
+    test_y
 )
 
 predictions = model.predict(test_x_orig)
-predictions
+
+def show_errors(test_x_orig, test_y, predictions):
+    images = 24
+    plt.figure(figsize=(32, 10))
+    counter = 1
+    for i, (prediction, gt) in enumerate(zip(predictions, test_y)):
+        #print(prediction.shape, gt.shape)
+        if counter <= images:
+            if np.abs(np.argmax(prediction) - np.argmax(gt)) > 0.1:
+                plt.title(f'image of {classes[np.argmax(gt)]}')
+                plt.subplot(counter // 6 + 1, 6, counter % 6 + 1)
+                plt.imshow(test_x_orig[i])
+                counter+=1
+    plt.show()
+
+show_errors(test_x_orig, test_y, predictions)
